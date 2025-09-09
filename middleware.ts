a631b4b1694fd9716e215b3b0930875b12cce196
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-09-09 13:48:14 +0000

    mise à jour du middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import createIntlMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
-import { auth } from "@/lib/auth";
+import { getToken } from "next-auth/jwt"; // ✅ Edge-compatible
 
 export const publicRoutes = ["/", "/actualites-nationales", "/articles", "/podcasts", "/galerie", "/a-la-une", "/sports", "/dailies"];
 
@@ -10,17 +10,22 @@ const intlMiddleware = createIntlMiddleware(routing);
 
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const session = await auth();
 
   const pathWithoutLocale = pathname.replace(/^\/[a-z]{2}(?=\/|$)/, '') || '/';
 
+  // Récupère ton token JWT (généré par NextAuth dans tes callbacks)
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
   if (process.env.NODE_ENV === "production") {
     console.log(`Page dans le middleware : "${pathWithoutLocale}"`)
-    console.log(session)
+    console.log(token)
   };
 
   if (pathWithoutLocale.startsWith("/api/auth")) {
-    return;
+    return NextResponse.next();
   }
 
   const isPublicPage = publicRoutes.some(route => pathWithoutLocale === route || pathWithoutLocale.startsWith(route + '/'));
@@ -28,14 +33,14 @@ export default async function middleware(req: NextRequest) {
   if (isPublicPage) {
     return intlMiddleware(req);
   } else {
-    if (!session) {
+    if (!token) {
       let callbackUrl = pathname;
       if (req.nextUrl.search) {
         callbackUrl += req.nextUrl.search;
       }
 
       const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-      const loginUrl = new URL(`/?callbackUrl=${encodedCallbackUrl}`, req.nextUrl.origin);
+      const loginUrl = new URL(`/?callbackUrl=${encodedCallbackUrl}`, req.url);
 
       return Response.redirect(loginUrl);
     }
